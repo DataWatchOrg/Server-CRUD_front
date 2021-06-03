2f5e15b4c1e491072e8e09779e71f7219c5d86a4
Ana Simplicio <asimplicio37@gmail.com>
2021-06-03 19:20:00 -0300

    Tela login estática

---
+++ data-watch/src/components/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="logo">
+        <v-icon color="primary" class="mr-1" :x-large="!text">mdi-eye-circle</v-icon>
+        <span v-if="text">
+            <span class="logo-data">Data</span>Watch
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+ .logo {
+    font-size: 27px;
+    color: #1976d2;
+    font-weight: 300;
+    margin: 10px;
+  }
+
+  .logo-data {
+    font-weight: 700;
+  }
+
+  .logo-icon-huge {
+      font-size: 35px;
+  }
+</style>
--- data-watch/src/views/Home.vue
+++ data-watch/src/views/Home.vue
@@ -1,15 +1,60 @@
 <template>
-  <hello-world />
+  <!-- <hello-world /> -->
+  <v-container class="login-container" fluid fill-height>
+    <v-row justify="center">
+      <v-card class="pa-4" max-width="300px">
+        <v-row justify="center">
+          <v-card-title>
+            <logo text/>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                dense
+                outlined
+                label="E-mail"
+              />
+              <v-text-field
+                dense
+                outlined
+                :type="'password'"
+                label="Senha"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              depressed
+              class="px-4 entrar-button"
+              color="blue-grey"
+            >
+              Entrar
+            </v-btn>
+          </v-card-actions>
+        </v-row>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import Logo from '../components/Logo'
 
   export default {
     name: 'Home',
-
     components: {
-      HelloWorld,
-    },
+      Logo
+    }
   }
 </script>
+
+<style scoped>
+  .login-container {
+    background-color: #1976d2;
+    overflow: hidden;
+  }
+
+  .entrar-button {
+    color: white !important;
+  }
+</style>
