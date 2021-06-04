95836e8ad920e17e113cbb7134af330d1191be66
oliveiramclaraa <oliveira.mclaraa@gmail.com>
2021-06-03 22:51:28 -0300

    exclui componente relatorio

--- data-watch/src/views/Relatorio.vue
+++ data-watch/src/views/Relatorio.vue
@@ -1,15 +1,52 @@
 <template>
-  <relatorio />
+  <v-card>
+    <v-card-title>
+      Relatório
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Busca"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="registros"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
-
 <script>
-  import Relatorio from '../components/Relatorio'
+  import axios from 'axios'
 
   export default {
     name: 'RelatorioView',
-
-    components: {
-      Relatorio,
+    data () {
+      return {
+        search: '',
+        headers: [
+          { text: 'Campos alterados', 
+            align: 'start',
+            value: 'campos_alterados' },
+          { text: 'Id Usuário', value: 'id_usuario' },
+          { text: 'Operação', value: 'operacao' },
+          { text: 'Id Operador', value: 'id_operador' },
+        ],
+        registros: []
+      }
     },
+    mounted (){
+      axios.get('main/listardocumentos',
+        { headers: { Accept: 'application/json'} })
+        .then(res => {
+            this.registros = res.data;
+            console.log(res.data);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
   }
 </script>
